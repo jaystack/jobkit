0c514f4911c3f959e4fcc0d4928a6e3cfe01feb0
Péter Hauszknecht <peter.hauszknecht@guidesmiths.com>
2017-07-21 17:37:45 +0200

    update git api

--- src/provider/apis/git.ts
+++ src/provider/apis/git.ts
@@ -2,7 +2,7 @@ import git = require('simple-git/promise')
 import { JobInfo } from '../../types'
 
 export default (jobInfo: JobInfo) => {
-  const defaultApi = git(jobInfo.cwd)
-  defaultApi['cwd'] = (cwd: string) => git(cwd)
-  return defaultApi
+  const commands = git(jobInfo.cwd)
+  Object.keys(commands).forEach(name => (git[name] = commands[name]))
+  return git
 }
